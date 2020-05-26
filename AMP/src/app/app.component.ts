0dac1a4a355cdfad3f859451ef48e0f16895c78f
Rick Donalson <rick.donalson@me.com>
2020-05-25 21:04:32 -0400

    commit

--- AMP/src/app/app.component.ts
+++ AMP/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  // tslint:disable-next-line: no-inferrable-types
+  pageTitle: string = 'Angular: Getting Started';
 }
