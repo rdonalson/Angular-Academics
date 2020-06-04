bc398a87a8f40413421332b250f0415d2862d37a
Rick Donalson <rick.donalson@me.com>
2020-06-04 12:43:31 -0400

    c

--- AMP/src/app/shared/components/star/star.component.ts
+++ AMP/src/app/shared/components/star/star.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.scss']
 })
-export class StarComponent implements OnInit {
-
+export class StarComponent implements OnInit, OnChanges {
+  @Input() rating: number;
+  starWidth: number;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 75 / 5;
   }
 
 }
