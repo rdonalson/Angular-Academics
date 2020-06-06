802c94b0fca313e29e305b4ce31da3ba90cd298b
Rick Donalson <rick.donalson@me.com>
2020-06-06 17:13:24 -0400

    c

--- AMP/src/app/app-routing.module.ts
+++ AMP/src/app/app-routing.module.ts
@@ -8,13 +8,16 @@ import { PageNotFoundComponent } from './system/page-not-found/page-not-found.co
 import { Paths } from './shared/models/paths';
 import { PathResolveService } from './shared/services/path-resolve.service';
 import { GlobalErrorComponent } from './system/error/global-error/global-error.component';
+import { ProductDetailGuard } from './shared/guards/product-detail.guard';
 
 
 const routes: Routes = [
   { path: Paths.home, component: WelcomeComponent },
   { path: Paths.error, component: GlobalErrorComponent },
   { path: Paths.products, component: ProductListComponent },
-  { path: Paths.products + '/:id', component: ProductDetailComponent },
+  { path: Paths.products + '/:id',
+    canActivate: [ProductDetailGuard],
+    component: ProductDetailComponent },
   { path: '', redirectTo: Paths.home, pathMatch: 'full' },
   { path: '**', resolve: {
       path: PathResolveService
