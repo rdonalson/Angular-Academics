e67586d3507ffc020bd2e0310204a8cef385ff9e
Rick Donalson <rick.donalson@me.com>
2020-06-16 15:22:58 -0400

    c

--- Angular-Routing-Example/projects/help-area/e2e/src/app.e2e-spec.ts
+++ Angular-Routing-Example/projects/help-area/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Angular-Routing-Example');
+    expect(page.getTitleText()).toEqual('help-area app is running!');
   });
 
   afterEach(async () => {
