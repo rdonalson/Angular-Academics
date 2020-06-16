3bb567f3df55c17bfabe3d14996342b153017002
Rick Donalson <rick.donalson@me.com>
2020-06-16 12:53:23 -0400

    c

--- Angular-Routing-Example/src/app/admin/users/users.component.ts
+++ Angular-Routing-Example/src/app/admin/users/users.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
+  users: any[];
+  // tslint:disable-next-line: no-inferrable-types
+  hasPermission: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
+    if (this.hasPermission) {
+      this.getUsers()
+        .then(users => this.users = users)
+        .catch(e => console.log(e.message));
+    } else {
+      this.users = [];
+    }
+  }
+
+  async getUsers() {
+    return [
+      { name: 'john', email: 'john@angular.com'},
+      { name: 'colleen', email: 'colleen@angular.com'}
+    ];
   }
 
 }
