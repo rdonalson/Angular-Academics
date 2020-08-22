5d91f14c2fcaa7acd6babbafff3a522feda501e7
Rick Donalson <rick.donalson@me.com>
2020-08-22 15:58:38 -0400

    c

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
   SessionListComponent,
   UpvoteComponent,
   VoterService,
+  LocationValidatorDirective,
   DurationPipe
 } from './events/index';
 
@@ -57,6 +58,7 @@ const jQuery = window['$'];
     CreateSessionComponent,
     SessionListComponent,
     CollapsibleWellComponent,
+    LocationValidatorDirective,
     UpvoteComponent,
     DurationPipe,
     SimpleModalComponent,
---
+++ ng-fundamentals/src/app/events/location-validator.directive.ts
@@ -0,0 +1,41 @@
+import { Directive } from '@angular/core';
+import {
+  Validator,
+  AbstractControl,
+  ValidationErrors,
+  FormGroup,
+  NG_VALIDATORS
+} from '@angular/forms';
+
+@Directive({
+  selector: '[appValidateLocation]',
+  providers: [{
+    provide: NG_VALIDATORS, useExisting: LocationValidatorDirective, multi: true
+  }]
+})
+export class LocationValidatorDirective implements Validator {
+  constructor() {}
+  validate(formGroup: FormGroup): { [key: string]: any } {
+    const addressControl = formGroup.controls.address;
+    const cityControl = formGroup.controls.city;
+    const countryControl = formGroup.controls.country;
+    const onlineUrlControl = (formGroup.root as FormGroup).controls.onlineUrl;
+
+    if (
+      (addressControl &&
+        addressControl.value &&
+        cityControl &&
+        cityControl.value &&
+        countryControl &&
+        countryControl.value) ||
+      (onlineUrlControl && onlineUrlControl.value)
+    ) {
+      return null;
+    } else {
+      return {validateLoction: false};
+    }
+  }
+  registerOnValidatorChange?(fn: () => void): void {
+    throw new Error('Method not implemented.');
+  }
+}
