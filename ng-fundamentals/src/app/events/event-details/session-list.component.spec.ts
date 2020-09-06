e5c7515f586c4057e85ea37a86c24bc108f4473e
Rick Donalson <rick.donalson@me.com>
2020-09-06 18:01:59 -0400

    c

---
+++ ng-fundamentals/src/app/events/event-details/session-list.component.spec.ts
@@ -0,0 +1,62 @@
+import { SessionListComponent } from './session-list.component';
+import { ISession } from '../shared/event.model';
+import { SimpleChanges } from '@angular/core';
+import { AuthService } from 'src/app/user/auth.service';
+import { VoterService } from './voter.service';
+
+
+
+describe('SessionListComponent', () => {
+  let component: SessionListComponent;
+  const changes: SimpleChanges = {};
+  const mockAuthService: AuthService = undefined;
+  const mockVoterService: VoterService = undefined;
+
+  beforeEach(() => {
+    component = new SessionListComponent(mockAuthService, mockVoterService);
+  });
+
+  describe('ngOnChanges', () => {
+    it('should filter the sessions correctly', () => {
+      component.sessions = ([
+        {name: 'session 1', level: 'intermediate'},
+        {name: 'session 2', level: 'intermediate'},
+        {name: 'session 3', level: 'beginner'}
+      ] as ISession[]);
+      component.filterBy = 'intermediate';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges(changes);
+      expect(component.visibleSessions.length).toBe(2);
+    });
+
+    it('should sort the sessions correctly', () => {
+      component.sessions = ([
+        {name: 'session 1', level: 'intermediate'},
+        {name: 'session 3', level: 'intermediate'},
+        {name: 'session 2', level: 'beginner'}
+      ] as ISession[]);
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges(changes);
+      expect(component.visibleSessions[2].name).toBe('session 3');
+    });
+  });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
