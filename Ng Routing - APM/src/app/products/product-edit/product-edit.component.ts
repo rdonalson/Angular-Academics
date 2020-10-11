b31f447756da37bd4a8c27bc80be4feada6f5d0c
Rick Donalson <rick.donalson@me.com>
2020-10-11 16:52:10 -0400

    Child Routes

--- Ng Routing - APM/src/app/products/product-edit/product-edit.component.ts
+++ Ng Routing - APM/src/app/products/product-edit/product-edit.component.ts
@@ -13,13 +13,15 @@ import { ProductService } from '../product.service';
 export class ProductEditComponent implements OnInit {
   pageTitle = 'Product Edit';
   errorMessage: string;
-
   product: Product;
+  private dataIsValid: { [key: string]: boolean } = {};
 
-  constructor(private productService: ProductService,
-              private messageService: MessageService,
-              private route: ActivatedRoute,
-              private router: Router) { }
+  constructor(
+    private productService: ProductService,
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
@@ -57,8 +59,17 @@ export class ProductEditComponent implements OnInit {
     }
   }
 
+  isValid(path?: string): boolean {
+    this.validate();
+    if (path) {
+      return this.dataIsValid[path];
+    }
+    return (this.dataIsValid
+      && Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
+  }
+
   saveProduct(): void {
-    if (true === true) {
+    if (this.isValid()) {
       if (this.product.id === 0) {
         this.productService.createProduct(this.product).subscribe({
           next: () => this.onSaveComplete(`The new ${this.product.productName} was saved`),
@@ -81,5 +92,27 @@ export class ProductEditComponent implements OnInit {
     }
     // Navigate back to the product list
     this.router.navigate(['/products']);
+  }
+
+  validate(): void {
+    // Clear the validation object
+    this.dataIsValid = {};
+
+    // 'info' tab
+    if (this.product.productName
+      && this.product.productName.length >= 3
+      && this.product.productCode) {
+      this.dataIsValid.info = true;
+    } else {
+      this.dataIsValid.info = false;
+    }
+
+    // 'tags' tab
+    if (this.product.category
+      && this.product.category.length >= 3) {
+      this.dataIsValid.tags = true;
+    } else {
+      this.dataIsValid.tags = false;
+    }
   }
 }
--- Ng Routing - APM/src/app/products/product.module.ts
+++ Ng Routing - APM/src/app/products/product.module.ts
@@ -6,6 +6,8 @@ import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductResolver } from './product-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 @NgModule({
   imports: [
@@ -21,6 +23,11 @@ import { ProductResolver } from './product-resolver.service';
         path: 'products/:id/edit',
         component: ProductEditComponent,
         resolve: { resolvedData: ProductResolver },
+        children: [
+          { path: '', redirectTo: 'info', pathMatch: 'full' },
+          { path: 'info', component: ProductEditInfoComponent },
+          { path: 'tags', component: ProductEditTagsComponent }
+        ]
       },
     ]),
   ],
@@ -28,6 +35,8 @@ import { ProductResolver } from './product-resolver.service';
     ProductListComponent,
     ProductDetailComponent,
     ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ],
 })
 export class ProductModule { }
