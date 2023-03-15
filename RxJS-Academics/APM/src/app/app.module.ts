87a12f0f4c5c29699491f35359283975f5eb1bf5
Rick Donalson <rick.donalson@me.com>
2023-03-15 13:17:08 -0400

    Created new version of APM Angular 15

---
+++ RxJS-Academics/APM/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppData } from './app-data';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(AppData, { delay: 1000 }),
+    AppRoutingModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
