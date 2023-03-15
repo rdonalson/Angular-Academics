5264ee14bcc2699fd742c88a89b8c043a585d542
Rick Donalson <rick.donalson@me.com>
2023-03-15 17:12:11 -0400

    Data Mapping Part A

--- RxJS-Academics/APM/src/app/products/product.service.ts
+++ RxJS-Academics/APM/src/app/products/product.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 
 import { Product } from './product';
 
@@ -12,15 +11,18 @@ export class ProductService {
   private productsUrl = 'api/products';
   private suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  products$ = this.http.get<Product[]>(this.productsUrl)
+    .pipe(
+      map(products => products.map(product => ({
+        ...product,
+        price: product.price ? product.price * 1.5 : 0,
+        searchKey: [product.productName]
+      } as Product))),
+      tap((data): void => console.log('Products: ', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
-      .pipe(
-        tap(data => console.log('Products: ', JSON.stringify(data))),
-        catchError(this.handleError)
-      );
-  }
+  constructor(private http: HttpClient) { }
 
   private fakeProduct(): Product {
     return {
