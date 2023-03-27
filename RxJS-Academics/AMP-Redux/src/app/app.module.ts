72722a23b97c3a644d2a9ab6c68a8397a589bbee
Rick Donalson <rick.donalson@me.com>
2023-03-27 17:38:17 -0400

    Added AMP-Redux, removed unused projects

---
+++ RxJS-Academics/AMP-Redux/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './home/menu.component';
+import { PageNotFoundComponent } from './home/page-not-found.component';
+import { ShellComponent } from './home/shell.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductData } from './products/product-data';
+import { UserModule } from './user/user.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(ProductData),
+    UserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    ShellComponent,
+    MenuComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
